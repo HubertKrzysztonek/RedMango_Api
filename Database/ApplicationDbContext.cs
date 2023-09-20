be1c978a214924a3d546b495c20c78735a7f6bb1
HubertKrzysztonek <hubertkrzysztonekk@gmail.com>
2023-09-20 20:21:01 +0200

    Added migartions + updated applicationUser

---
+++ Database/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using RedMango_Api.Models;
+
+namespace RedMango_Api.Database
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options) 
+        {
+        
+        }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
 
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using RedMango_Api.Database;
+using RedMango_Api.Models;
+
 namespace RedMango_Api
 {
     public class Program
@@ -8,6 +13,11 @@ namespace RedMango_Api
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultDbConnection"));
+            });
+            builder.Services.AddIdentity<ApplicationUser,IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
