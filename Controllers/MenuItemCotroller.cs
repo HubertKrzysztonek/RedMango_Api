1dac57cf6ecfad708ed524481da9fd88b3e9fb10
HubertKrzysztonek <hubertkrzysztonekk@gmail.com>
2023-09-24 13:01:50 +0200

    Added MenuItemController and ApiResponse

---
+++ Controllers/MenuItemCotroller.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RedMango_Api.Database;
+using RedMango_Api.Models;
+using System.Net;
+
+namespace RedMango_Api.Controllers
+{
+    [Route("api/MenuItem")]
+    [ApiController]
+    public class MenuItemCotroller :ControllerBase
+    {
+        private readonly ApplicationDbContext _dbcontext;
+        private ApiResponse _response;
+
+        public MenuItemCotroller(ApplicationDbContext dbcontext)
+        {
+            _dbcontext = dbcontext;
+            _response = new ApiResponse();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetMenuItems()
+        {
+            _response.Result = _dbcontext.MenuItems;
+            _response.StatusCode = HttpStatusCode.OK;
+            return Ok(_response);
+        }
+    }
+}
